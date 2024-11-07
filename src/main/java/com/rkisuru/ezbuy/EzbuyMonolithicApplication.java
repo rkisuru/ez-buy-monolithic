21926cb61e94e93721b45fca1fbee0726aad1f9a
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-11-07 22:54:36 +0530

    init project

---
+++ src/main/java/com/rkisuru/ezbuy/EzbuyMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.rkisuru.ezbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzbuyMonolithicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzbuyMonolithicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkisuru/ezbuy/EzbuyMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkisuru.ezbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzbuyMonolithicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
