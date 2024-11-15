5bc3db434d8171b59f08243dd2e112b7b8883dbd
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-11-15 12:05:14 +0530

    fix: constructors, getters, setters

--- src/main/java/com/rkisuru/ezbuy/product/ProductResponse.java
+++ src/main/java/com/rkisuru/ezbuy/product/ProductResponse.java
@@ -1,9 +1,14 @@
 package com.rkisuru.ezbuy.product;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import java.math.BigDecimal;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponse {
 
     private Long id;
